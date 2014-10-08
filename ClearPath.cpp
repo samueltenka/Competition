7d4d53267cc9a7f376c03697b4df18e902fabbc3
samueltenka <iguavadon@gmail.com>
2014-10-08 14:10:15 -0700

    Update ClearPath.cpp

--- ClearPath.cpp
+++ ClearPath.cpp
@@ -5,33 +5,45 @@ using namespace std;
 
 int M, N;
 int** heights;
-void new_heights() {
-   heights = new int*[M];
+int** travel_times;
+int** new_array() {
+   int** arr = new int*[M];
    for(int i = 0; i < M; i++) {
-      heights[i] = new int[N];
+      arr[i] = new int[N];
    }
-} void get_heights() {
+} void init_arrays() {
    cin >> M, N;
+   for(int i = 0; i < M; i++) {
+      for(int j = 0; j < N; j++) {
+         cin >> heights[i][j]; 
+         travel_times[i][j] = 101;
+      }
+   }
+} void delete_array(int** arr) {
+   for(int i = 0; i < M; i++) {
+      delete arr[i];
+   }
+   delete[] arr;
+}
+
+void revised_time(int i, int j) {
+   return 
+}
+void fill_() {
    for(int i = 0; i < M; i++) {
       for(int j = 0; j < N; j++) {
          cin >> heights[i][j];
       }
    }
-} void delete_heights() {
-   for(int i = 0; i < M; i++) {
-      delete heights[i];
-   }
-   delete[] heights;
-}
-
-void fill_heights() {
-
 } // Strategy: dynamic programming :)
 
 void main() {
-   new_heights();
-   get_heights();
-   fill_heights();
+   heights = new_array();
+   travel_times = new_array();
+   init_arrays();
+
    cout << heights[M-1][N-1];
-   delete_heights();
+
+   delete_array(travel_times);
+   delete_array(heights);
 }
